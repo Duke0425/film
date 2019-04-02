ef153fa09d4fac0beb8f1936c9ea96e7d4903955
posion123 <1067269320@qq.com>
2019-04-02 14:30:23 +0800

    基本搭建

---
+++ f_project/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from users import views
+
+urlpatterns = [
+    path('hello/', views.hello, name='hello'),
+    path('index/', views.index, name='index'),
+]
--- f_project/users/views.py
+++ f_project/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello(request):
+    return HttpResponse('OK')
+
+def index(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
